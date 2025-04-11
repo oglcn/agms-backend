d1ff08470b8ac0621c288dd43a343bf319108ed2
Yigit Sariyar <yigitsariyar23@gmail.com>
2025-04-11 22:09:46 +0300

    spring init

---
+++ backend/src/main/java/com/agms/backend/AgmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agms.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgmsBackendApplication.class, args);
+	}
+
+}
