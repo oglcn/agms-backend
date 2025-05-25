42c38ed7a5a3077726b34d94252e8796bc8a2559
Yigit Sariyar <yigitsariyar23@gmail.com>
2025-05-26 00:18:39 +0300

    cors fix

--- src/main/java/com/agms/backend/config/WebConfig.java
+++ src/main/java/com/agms/backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://agms-frontend.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
